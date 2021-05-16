c29de3f3521a90aed72d04856388ade8e7b065a7
angelcamposm <angel.campos.m@outlook.com>
2021-05-16 23:19:17 +0200

    Added a command to publish vendor assets

---
+++ src/Console/Commands/InstallPackage.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Acamposm\SnmpPoller\Commands;
+
+use Illuminate\Console\Command;
+
+class InstallPackage extends Command
+{
+    protected $signature = 'ping:install';
+
+    protected $description = 'Install the Ping package';
+
+    public function handle()
+    {
+        if (file_exists(config_path('snmp.php'))) {
+            $this->hidden = true;
+        }
+
+        $this->info('Installing Snmp Poller Package...');
+
+        $this->info('Publishing configuration...');
+
+        $this->call('vendor:publish', [
+            '--provider' => 'Acamposm\SnmpPoller\SnmpPollerServiceProvider',
+            '--tag' => 'config',
+        ]);
+
+        $this->info('Snmp Poller Package installed.');
+    }
+}
