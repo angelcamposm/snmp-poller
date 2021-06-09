fe38857957f7f122c2a9f9b5f8e54a1d64ea7f63
angelcamposm <angel.campos.m@outlook.com>
2021-06-09 09:44:47 +0200

    WIP: Added descriptions on methods of the Interface.

--- src/Interfaces/SnmpPollerInterface.php
+++ src/Interfaces/SnmpPollerInterface.php
@@ -5,21 +5,29 @@ namespace Acamposm\SnmpPoller\Interfaces;
 interface SnmpPollerInterface
 {
     /**
+     * Return an array with SNMP OIDs to be queried.
+     *
      * @return array
      */
     public function getOids(): array;
 
     /**
+     * Return the snmp table name associated with the poller.
+     *
      * @return string
      */
     public function getTable(): string;
 
     /**
+     * Return an object with information about the poller.
+     *
      * @return object
      */
     public function info(): object;
 
     /**
+     * Check if SNMP poller is a table or a leaf.
+     *
      * @return bool
      */
     public function isTable(): bool;
