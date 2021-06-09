49080f14c0f1bb80e7e536476cbe59f9a13386cb
angelcamposm <angel.campos.m@outlook.com>
2021-06-09 09:45:22 +0200

    WIP: Updated SnmpPollerServiceProvider.php

--- src/Providers/SnmpPollerServiceProvider.php
+++ src/Providers/SnmpPollerServiceProvider.php
@@ -4,6 +4,7 @@ namespace Acamposm\SnmpPoller\Providers;
 
 use Acamposm\SnmpPoller\Console\Commands\InstallSnmpPackageCommand;
 use Acamposm\SnmpPoller\Console\Commands\MakeCustomPollerCommand;
+use Acamposm\SnmpPoller\Console\Commands\MakeSnmpPollerCommand;
 use Acamposm\SnmpPoller\Facades\SnmpPoller;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,17 +15,9 @@ class SnmpPollerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('snmp.php'),
-            ], 'config');
-
-            // Registering package commands.
-            $this->commands([
-                InstallSnmpPackageCommand::class,
-                MakeCustomPollerCommand::class,
-            ]);
-        }
+        $this->publishConfig();
+        $this->publishMigrations();
+        $this->registerCommands();
     }
 
     /**
@@ -35,5 +28,48 @@ class SnmpPollerServiceProvider extends ServiceProvider
         $this->app->bind('snmp-poller', function ($app) {
             return new SnmpPoller();
         });
+    }
+
+    /**
+     * Register commands for the package.
+     *
+     * @return void
+     */
+    private function registerCommands(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                InstallSnmpPackageCommand::class,
+                MakeSnmpPollerCommand::class,
+            ]);
+        }
+
+        $this->commands([
+            MakeCustomPollerCommand::class,
+        ]);
+    }
+
+    /**
+     * Publish config file for the package.
+     *
+     * @return void
+     */
+    private function publishConfig()
+    {
+        $this->publishes([
+            __DIR__.'/../../config/config.php' => config_path('snmp.php'),
+        ], 'config');
+    }
+
+    /**
+     * Publish migrations files for the package.
+     *
+     * @return void
+     */
+    private function publishMigrations(): void
+    {
+        $this->publishes([
+            __DIR__.'/../../database/migrations' => database_path('migrations'),
+        ], 'migrations');
     }
 }
